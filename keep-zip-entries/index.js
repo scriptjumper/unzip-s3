705626268137f131b1e39f9646ab9e6c6a78961c
shaeen <shaeen@rapidtrade.biz>
2017-12-04 11:02:11 +0200

    keeps the zip files in the bucket

--- keep-zip-entries/index.js
+++ keep-zip-entries/index.js
@@ -1,36 +1,39 @@
 'use strict';
+let AWS, s3, Rx, AdmZip, bucket, file_key, file_size, slashidx, supplierID, params, zip, zipEntries, source, results;
 
-let AWS = require('aws-sdk');
-let s3 = new AWS.S3({apiVersion: '2006-03-01'});
-let Rx =  require('rx');
-let AdmZip = require('adm-zip');
-
+AWS = require('aws-sdk');
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+Rx =  require('rx');
+AdmZip = require('adm-zip');
 
 exports.handler = (event, context, callback) => {
   console.log('Node:' + JSON.stringify(process.versions));
   console.log('Received event:', JSON.stringify(event, null, 2));
 
-  let bucket = event.Records[0].s3.bucket.name;
-  let file_key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+  bucket = event.Records[0].s3.bucket.name;
+  file_key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+  file_size = event.Records[0].s3.object.size;
   console.log("\nFetching from " + bucket + "S3 bucket");
   console.log("\nFile to unzip is " + file_key);
-  let slashidx = file_key.indexOf('/');
-  let supplierID = file_key.slice(0,slashidx);
-
-  let params = { Bucket: bucket, Key: file_key };
+  console.log("\nFile size is " + file_size);
+  
+  slashidx = file_key.indexOf('/');
+  supplierID = file_key.slice(0,slashidx);
+  
+  params = { Bucket: bucket, Key: file_key };
   s3.getObject(params, (err, data) => {
     if (err) {
       callback(err, null);
     } else {
       if (!data) callback(null, 'No Data!');
-      let zip = new AdmZip(data.Body);
-      let zipEntries = zip.getEntries(); // ZipEntry objects
-      let source = Rx.Observable.from(zipEntries);
-      let results = [];
+      zip = new AdmZip(data.Body);
+      zipEntries = zip.getEntries(); // ZipEntry objects
+      source = Rx.Observable.from(zipEntries);
+      results = [];
 
       source.subscribe(
         (zipEntry) => {
-          let params = {
+          params = {
             Bucket  : bucket,
             Key     : supplierID + '/' + zipEntry.name,
             Body    : zipEntry.getData() // decompressed file as buffer
@@ -38,7 +41,7 @@ exports.handler = (event, context, callback) => {
           // upload decompressed file
           s3.putObject(params, (err, data) => {
             if (err) console.log(err, err.stack); // an error occurred
-            else results.push(data);           // successful response
+            else results.push(data, "successful");           // successful response
           });
         },
         (err) => {
